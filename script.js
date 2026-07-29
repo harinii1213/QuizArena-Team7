4cd7a4db3d8da82887b7bc5bec772b776a01a27d
jenifa1702-rjj <jenifa1702@gmail.com>
2026-07-29 18:31:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+console.log("Welcome to QuizArena!");
+const startButton = document.getElementById("startQuiz");
+if (startButton) 
+{
+startButton.addEventListener("click", function ()
+{
+window.location.href = "category.html";
+});
+}
+const leaderboardButton = document.getElementById("leaderboard");
 
+if (leaderboardButton) 
+{
+leaderboardButton.addEventListener("click", function () 
+{
+window.location.href = "leaderboard.html";
+});
+}
